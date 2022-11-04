0d49d38e25c58a2aaa157ee03a7e37fc6b50b1a7
Spencer Posner <spencer@thecamg.com>
2022-11-04 09:47:19 -0400

    sentence maker fn

--- textpro.py
+++ textpro.py
@@ -3,3 +3,13 @@
 # capitalize first letter & space between sentences
 # end when \end is entered
 # input prompt says "Say something: "
+
+def sentence_maker(phrase):
+    q_words = ("how", "what", "why", "who", "when", "where")
+    cap = phrase.capitalize()
+    
+    if phrase.startswith(q_words):
+        return f"{cap}?"
+    else:
+        return f"{cap}."
+    
